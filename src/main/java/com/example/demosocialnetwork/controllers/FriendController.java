d8c020d71aa82ee460735b28afdc21c79cc19c88
viviajohn <32187505+viviajohn@users.noreply.github.com>
2020-11-01 00:41:49 +0530

    Update FriendCnotroller.java
    new changes

--- src/main/java/com/example/demosocialnetwork/controllers/FriendController.java
+++ src/main/java/com/example/demosocialnetwork/controllers/FriendController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -124,6 +125,13 @@ public class FriendController {
 			
 			return new ResponseEntity<FriendDto>(HttpStatus.NOT_FOUND);
 		
+	}
+	
+	@DeleteMapping("/{friendId}")
+	public ResponseEntity<FriendDto> deleteFriend(@PathVariable("friendId") Integer friendId) {
+		friendRepo.deleteById(friendId);
+		
+		return new ResponseEntity<FriendDto>(HttpStatus.NO_CONTENT);
 	}
 
 }
